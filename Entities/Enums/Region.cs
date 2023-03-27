28b961def03657f629c2633cb3ee964d29c86013
nikola.golijanin <nikola.golijanin@itengine.rs>
2023-03-27 18:06:26 +0200

    Added Case and Region

---
+++ Entities/Enums/Region.cs
@@ -0,0 +1,17 @@
+﻿namespace Entities.Enums;
+
+public enum Region
+{
+    LJ,
+    CE,
+    KR,
+    NM,
+    KK,
+    KP,
+    MB,
+    MS,
+    NG,
+    PO,
+    SG,
+    ZA
+}
---
+++ Entities/Models/Case.cs
@@ -0,0 +1,13 @@
+﻿namespace Entities.Models;
+
+public class Case
+{
+    public DateTime Date { get; set; }
+    
+    public int? NumberOfActiveCases { get; set; }
+    
+    public int? NumberOfVacinatedFirst { get; set; }
+    
+    public int? NumberOfVacinatedSecond { get; set; }
+    public int? DeceasedToDate { get; set; }
+}
